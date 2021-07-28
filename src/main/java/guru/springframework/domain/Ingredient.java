9e8b4db017a7791954be72ecfbaf9b8856756824
Sunpil Kim <ezweb28@gmail.com>
2021-07-28 01:06:45 -0400

    CH205. Crate Ingredient

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -1,12 +1,14 @@
 package guru.springframework.domain;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-@Data
+@Getter
+@Setter
 @EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
@@ -30,6 +32,13 @@ public class Ingredient {
         this.descriptions = descriptions;
         this.amount = amount;
         this.uom = uom;
+    }
+
+    public Ingredient(String descriptions, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.descriptions = descriptions;
+        this.amount = amount;
+        this.uom = uom;
+        this.recipe = recipe;
     }
 
 }
